ac27b29f2e66aceb567441eae96b60ad02748e6c
Robbie <mccutcheon.robbie@gmail.com>
2026-02-28 13:18:18 -0500

    completed catalog feature

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,7 +1,29 @@
+import data from "./Data";
+
 function Products() {
   return (
-    <div>
-      <h2>Products</h2>
+    <div className="content">
+      <ul className="products">
+        {data.products.map((product) => (
+          <li key={product.name}>
+            <div className="product">
+              <img
+                className="product-image"
+                src={product.imageUrl}
+                alt={product.name}
+              />
+              <div className="product-name">
+                <a href="product.html">{product.name}</a>
+              </div>
+              <div className="product-brand">{product.brand}</div>
+              <div className="product-price">${product.price}</div>
+              <div className="product-rating">
+                {product.rating} Stars ({product.numberOfReviews} Reviews)
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
